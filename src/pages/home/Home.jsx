94fb5ee2963527bc5e1c2f6d1fca329ccde58d4e
dungpro2011 <75030627+dungpro2011@users.noreply.github.com>
2022-07-09 23:40:46 +0700

    Add special in Pages

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,6 +7,7 @@ import { Search } from "../search/Search";
 import Topic from "../topic/Topic";
 import MoviesTrending from "../movies_trending/MoviesTrending";
 import Shows from "../tv_series/Shows";
+import Special from "../special/Special";
 
 export default function Home() {
   useEffect(() => {
@@ -230,67 +231,25 @@ export default function Home() {
 
       <Search/>
       <Menu/>
-
       {/*END NAV */}
-      {/* SLIDE SECTION */}
 
+      {/* SLIDE SECTION */}
       <Topic/>
-      
       {/*END SLIDE SECTION */}
+
       {/* MOVIES TRENDING */}
       <MoviesTrending/>
-      
       {/* END MOVIES TRENDING */}
+
       {/* TV SERIES */}
       <Shows/>
-      {/* TV SERIES */}
+      {/*END TV SERIES */}
+      
       {/* SPECIAL MOVIES */}
-      <div className="special">
-        <div className="section-wrapper">
-          <div className="section-header">hot movie</div>
-          <div className="big-slide-item special-movie active to-top show-on-scroll">
-            <img
-              src={require("../../assets/img/Images/black-banner.png")}
-              alt="Poster"
-            />
-            <div className="big-slide-item-content">
-              <div className="item-content-wrapper">
-                <div className="item-content-title ">black Panther</div>
-                <div className="movies-infors">
-                  <div className="movies-infor">
-                    <ion-icon name="bookmark-outline" />
-                    <span>9.5</span>
-                  </div>
-                  <div className="movies-infor">
-                    <ion-icon name="time-outline" />
-                    <span>120 mins</span>
-                  </div>
-                  <div className="movies-infor">
-                    <ion-icon name="cube-outline" />
-                    <span>FHD</span>
-                  </div>
-                </div>
-                <div className="item-content-description ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-                  possimus eius. Deserunt non odit, cum vero reprehenderit
-                  laudantium odio vitae autem quam, incidunt molestias ratione
-                  mollitia accusantium, facere ab suscipit.
-                </div>
-              </div>
-            </div>
-            <div className="play-movies ">
-              <div className="ring" />
-              <a href="https://www.youtube.com/watch?v=qZuQc-GvRlk">
-                <i className="bx bxs-right-arrow" />
-              </a>
-              <div className="btn-watch">
-                <span>watch trailer</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Special/>
       {/*END SPECIAL MOVIES */}
+
+      {/* FOOTER */}
       <Footer/>
       
     </div>
